3dea3c03d6e3b7ef76bb32647931225fcc3e5715
MASTERCHIEF07 <kjonathan1999@hotmail.com>
2024-11-08 15:25:36 +0300

    STARTED TESTING GUI

--- test/snake_run.cpp
+++ test/snake_run.cpp
@@ -2,13 +2,9 @@
 #include <snake_engine/engine.hpp>
 
 int main(){
-    auto sn = se::Snake{};
-    auto pg = se::PlayGround{800, 400};
+    //auto sn = se::Snake{};
+    //auto pg = se::PlayGround{800, 400};
     auto engine = se::Engine{"snake", 800, 400};
-
-    engine
-        .setSnake(se::Snake{})
-        .setPlayGround(se::PlayGround{800, 400});
 
     return 0;
 }
